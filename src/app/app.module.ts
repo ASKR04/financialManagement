86669506fb882fba2a23ad674176cbcca60a038e
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2024-05-17 15:04:22 -0500

    BPC

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+})
+export class AppModule { }
