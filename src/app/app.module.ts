a0a25a80d4a05e5af223e4053e8ad206cb16716b
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2024-05-17 15:06:32 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    RegistrationComponent
   ],
   imports: [
     BrowserModule
